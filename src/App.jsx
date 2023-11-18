dc0d37db6184282d5b9eabf57e451e223679ceca
sureshbabuvikram <sureshbabuvikram@gmail.com>
2023-11-18 19:28:52 +0530

    undefined

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import AuthPage from "./AuthPage";
 import ChatsPage from "./ChatsPage";
 
 function App() {
-  const [user, setUser] = useState(undefined);
+  const [user, setUser] = useState("undefined");
 
   if (!user) {
     return <AuthPage onAuth={(user) => setUser(user)} />;
