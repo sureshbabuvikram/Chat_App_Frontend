e10460d7b72c20b461b4a9f1279800d573fba855
sureshbabuvikram <sureshbabuvikram@gmail.com>
2023-11-18 20:03:06 +0530

    preety

--- src/ChatsPage.jsx
+++ src/ChatsPage.jsx
@@ -1,32 +1,32 @@
-// import React from 'react';
-// import {PrettyChatWindow} from 'react-chat-engine-pretty';
+import React from 'react';
+import {PrettyChatWindow} from 'react-chat-engine-pretty';
 
-// const ChatsPage = (props) => {
-//     return (
-//         <div style={{height:'100vh'}}>
-//             <PrettyChatWindow
-//             projectId='5753945a-6419-4e71-a8b7-d8958172fb05'
-//             username={props.user.username}
-//             secret={props.user.secret}
-//             style={{height:'100%'}}
-//             />
-//         </div>
-//     );
-// };
-
-// export default ChatsPage;
-
-
-import {MultiChatSocket,useMultiChatLogic,MultiChatWindow  } from 'react-chat-engine-advanced';
-  
-  const ChatsPage = (props) => {
-    const chatProps = useMultiChatLogic(
-      '5753945a-6419-4e71-a8b7-d8958172fb05',props.user.username,props.user.secret);
+const ChatsPage = (props) => {
     return (
-      <div style={{height:'100vh'}}>
-        <MultiChatWindow {...chatProps} />
-        <MultiChatSocket {...chatProps}  style={{height:'100%'}} />
-      </div>
+        <div style={{height:'100vh'}}>
+            <PrettyChatWindow
+            projectId='5753945a-6419-4e71-a8b7-d8958172fb05'
+            username={props.user.username}
+            secret={props.user.secret}
+            style={{height:'100%'}}
+            />
+        </div>
     );
-  }
-  export default ChatsPage;
+};
+
+export default ChatsPage;
+
+
+// import {MultiChatSocket,useMultiChatLogic,MultiChatWindow  } from 'react-chat-engine-advanced';
+  
+//   const ChatsPage = (props) => {
+//     const chatProps = useMultiChatLogic(
+//       '5753945a-6419-4e71-a8b7-d8958172fb05',props.user.username,props.user.secret);
+//     return (
+//       <div style={{height:'100vh'}}>
+//         <MultiChatWindow {...chatProps} />
+//         <MultiChatSocket {...chatProps}  style={{height:'100%'}} />
+//       </div>
+//     );
+//   }
+//   export default ChatsPage;
